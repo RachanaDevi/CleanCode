a819d83858ea12ad1a61a8d02ef2d049ad64c534
Rachana Devi <rachana.devi@thoughtworks.com>
2021-07-03 13:03:06 +0530

    [Rachana] | [Inlines Temp] | Refactor. Inlines temporary variable primaryAcc

--- src/main/java/refactoring/SplitTemporaryVariable.java
+++ src/main/java/refactoring/SplitTemporaryVariable.java
@@ -10,12 +10,11 @@ public class SplitTemporaryVariable {
     double secondaryForce = 4;
 
     public double getDistanceTravelled(int time) {
-        final double primaryAcc = primaryAcc();
         int primaryTime = Math.min(time, delay);
-        double result = 0.5 * primaryAcc * primaryTime * primaryTime;
+        double result = 0.5 * primaryAcc() * primaryTime * primaryTime;
         int secondaryTime = time - delay;
         if (secondaryTime > 0) {
-            double primaryVel = primaryAcc * delay;
+            double primaryVel = primaryAcc() * delay;
             final double secondaryAcc = (primaryForce + secondaryForce) / mass;
             result += primaryVel * secondaryTime + 0.5 * secondaryAcc * secondaryTime * secondaryTime;
         }
